c8da9f48451c24cf1e86042b3d77003870aced6e
Doheera-kosi <kupourevans99@gmail.com>
2023-01-25 14:09:33 +0000

    Add post.rb & mehtods for posts

---
+++ app/models/post.rb
@@ -0,0 +1,15 @@
+class < ApplicationRecord
+  include ApplicationHelper
+
+  belongs_to :user
+  has_many :likes
+  has_many :comments
+
+  def recent_comments
+    comments.limit(5).order(created_at: :desc)
+  end
+
+  def update_post_counter
+    user.increment!(:Post_Counter)
+  end
+end
