f487b528a0723cb2ee139a40928b9faf80840fe2
Doheera-kosi <kupourevans99@gmail.com>
2023-01-26 15:07:31 +0000

    Update like methods

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,8 +1,8 @@
 class Like < ApplicationRecord
-  belongs_to :user
   belongs_to :post
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
   def update_likes_counter
-    post.increment!(:LikesCounter)
+    update(likes_counter: likes.count)
   end
 end
