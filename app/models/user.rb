26202bb3f4794e9dc8ff36dcaa9edbe54842ba73
Doheera-kosi <kupourevans99@gmail.com>
2023-01-26 15:09:01 +0000

    Update user methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
-  has_many :posts
-  has_many :likes
-  has_many :comments
+  has_many :posts, dependent: :destroy, foreign_key: 'author_id'
+  has_many :comments, foreign_key: 'author_id'
+  has_many :likes, foreign_key: 'author_id'
 
-  def recent_users
-    posts.limit(3).order(created_at: :desc)
+  def last_three_posts
+    posts.last(3)
   end
 end
+
+app/models/
