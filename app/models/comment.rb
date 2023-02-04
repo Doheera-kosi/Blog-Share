9f08634244cd4bbf0059f581c4c7a6786ef991c3
Doheera-kosi <kupourevans99@gmail.com>
2023-02-04 00:27:26 +0000

    Update files

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,14 +1,3 @@
-# class Comment < ApplicationRecord
-#   belongs_to :post
-#   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-#   before_save :comment_counter
-
-#   validates :text, presence: true
-#   def update_comments_counter
-#     post.update(comments_counter: post.comments.all.length)
-#   end
-# end
-
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post, class_name: 'Post'
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,12 +1,3 @@
-# class Like < ApplicationRecord
-#   belongs_to :post
-#   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-
-#   def update_likes_counter
-#     post.update(likes_counter: post.likes.all.length)
-#   end
-# end
-
 class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post, class_name: 'Post'
